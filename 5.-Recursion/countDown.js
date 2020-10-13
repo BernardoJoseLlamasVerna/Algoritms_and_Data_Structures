8b4cb502f65d1e4cc9117b884f1ca6862c3efb6d
Bernardo José Llamas Verna <bernardo.llamas@sngular.com>
2020-10-13 18:42:20 +0200

    recursion tips

---
+++ 5.-Recursion/countDown.js
@@ -0,0 +1,16 @@
+function countDown(num) {
+    if (num <= 0) {
+        console.log("All done!");
+        return;
+    }
+    console.log(num);
+    num--;
+    countDown(num);
+}
+
+function countDown(num) {
+    for (var i = num; i > 0; i--) {
+        console.log(i);
+    }
+    console.log("All done!");
+}
