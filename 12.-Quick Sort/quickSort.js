817de13b2bba95b88ede429adc12e452acead7e7
Bernardo José Llamas Verna <bernardo.llamas@sngular.com>
2020-10-26 18:41:07 +0100

    Quick Sort Algorithm

---
+++ 12.-Quick Sort/quickSort.js
@@ -0,0 +1,54 @@
+// First Version
+/*function pivot(arr, start = 0, end = arr.length + 1) {
+    function swap(array, i, j) {
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    // pivot always start at the beginning:
+    var pivot = arr[start];
+    var swapIdx = start;
+
+    for (var i = start +1; i < arr.length; i++) {
+        if (pivot > arr[i]) {
+            swapIdx++;
+            swap(arr, swapIdx, i);
+        }
+    }
+    swap(arr, start, swapIdx);
+    return swapIdx;
+}*/
+
+// Version with ES2015 Syntax
+function pivot(arr, start = 0, end = arr.length - 1) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+
+    // We are assuming the pivot is always the first element
+    let pivot = arr[start];
+    let swapIdx = start;
+
+    for (let i = start + 1; i <= end; i++) {
+        if (pivot > arr[i]) {
+            swapIdx++;
+            swap(arr, swapIdx, i);
+        }
+    }
+
+    // Swap the pivot from the start the swapPoint
+    swap(arr, start, swapIdx);
+    return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right);
+        // left
+        quickSort(arr, left, pivotIndex - 1);
+        // right
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
