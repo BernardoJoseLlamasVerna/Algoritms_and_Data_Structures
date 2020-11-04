923b9341b45a91c87bf3839560c8e0b93eeb758f
Bernardo José Llamas Verna <bernardo.llamas@sngular.com>
2020-11-04 18:56:40 +0100

    more about Doubly Linked Lists

--- 16.-Doubly Linked Lists/doublyLinkedList.js
+++ 16.-Doubly Linked Lists/doublyLinkedList.js
@@ -12,4 +12,59 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    push(val) {
+        var newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop() {
+        if (!this.head) return undefined;
+        var currentTail = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = currentTail.prev;
+            this.tail.next = null;
+            currentTail.prev = null;
+        }
+
+        this.length--;
+        return currentTail;
+    }
+    shift() {
+        if(this.length === 0) return undefined;
+        var oldHead = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
+    unshift(val) {
+        var newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
