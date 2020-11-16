cf82b02a81623d6d40e059d50abad9e12868f232
Bernardo José Llamas Verna <bernardo.llamas@sngular.com>
2020-11-16 18:20:08 +0100

    finishing bin. heaps

--- 20.-Binary Heaps/priorityQueue.js
+++ 20.-Binary Heaps/priorityQueue.js
@@ -1,9 +1,9 @@
 class PriorityQueue {
     constructor() {
-        this.values = [41, 39, 33, 18, 27, 12];
+        this.values = [];
     }
-    insert(val, priority) {
-        let newNode = new Node(val, priority)
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority);
         this.values.push(newNode);
         this.bubbleUp();
     }
@@ -13,20 +13,20 @@ class PriorityQueue {
         while (idx > 0) {
             let parentIdx = Math.floor((idx - 1)/2);
             let parent = this.values[parentIdx];
-            if (element <= parent) break;
+            if (element.priority >= parent.priority) break;
             this.values[parentIdx] = element;
             this.values[idx] = parent;
             idx = parentIdx;
         }
     }
-    extractMax() {
-        const max = this.values[0];
+    dequeue() {
+        const min = this.values[0];
         const end = this.values.pop();
         if (this.values.length > 0) {
             this.values[0] = end;
             this.sinkDown();
         }
-        return max;
+        return min;
     }
     sinkDown() {
         let idx = 0;
@@ -40,15 +40,15 @@ class PriorityQueue {
 
             if (leftChildIdx < length) {
                 leftChild = this.values[leftChildIdx];
-                if (leftChild > element) {
+                if (leftChild.priority < element.priority) {
                     swap = leftChildIdx;
                 }
             }
             if (rightChildIdx < length) {
                 rightChild = this.values[rightChildIdx];
                 if (
-                    (swap === null && rightChild > element) ||
-                    (swap !== null && rightChild > leftChild)
+                    (swap === null && rightChild.priority < element.priority) ||
+                    (swap !== null && rightChild.priority < leftChild.priority)
                 ) {
                     swap = rightChildIdx;
                 }
@@ -67,3 +67,10 @@ class Node {
         this.priority = priority;
     }
 }
+
+let ER = new PriorityQueue();
+ER.enqueue("common cold", 5);
+ER.enqueue("gunshot wound", 1);
+ER.enqueue("high fever", 4);
+ER.enqueue("broken arm", 2);
+ER.enqueue("glass in foot", 3);
