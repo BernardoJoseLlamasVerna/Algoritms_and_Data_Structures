9b3d7026a872331261f9ff110dedf3a244d52cfd
Bernardo José Llamas Verna <bernardo.llamas@sngular.com>
2020-11-17 18:20:07 +0100

    hash tables end

--- 21.-Hash Tables/hashTables.js
+++ 21.-Hash Tables/hashTables.js
@@ -1,4 +1,4 @@
-function hashForStrings(key, arrayLen) {
+/*function hashForStrings(key, arrayLen) {
     let total = 0;
     let WEIRD_PRIME = 31;
     for (let i = 0; i < Math.min(key.length, 100); i++) {
@@ -7,4 +7,11 @@ function hashForStrings(key, arrayLen) {
         total = (total*WEIRD_PRIME + value) % arrayLen;
     }
     return total;
-}
+}*/
+
+
+
+let ht = new HashTable(17);
+ht.set("hello world", "goodbye!!");
+ht.set("cats", "good!!");
+ht.set("dogs", "bad!!");
